122fc26c84cbe5695340d4a0329b9fe7a3c56e2f
“tiaraaaaaa” <tiaraaaxie@gmail.com>
2025-04-27 22:24:58 +0900

    update website

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1745655345',resources: [
+    cacheName: 'chirpy-1745760265',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
