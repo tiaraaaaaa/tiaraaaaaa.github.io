0712202645c5a6ba785b12f460d5eea40ee3ddac
“tiaraaaaaa” <tiaraaaxie@gmail.com>
2026-01-21 15:43:29 +0800

    250121

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1768971973',resources: [
+    cacheName: 'chirpy-1768981386',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
